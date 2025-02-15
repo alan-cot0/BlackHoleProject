ae824e44553741db88d36d2194178d1e0862681b
alan-cot0 <160337699+alan-cot0@users.noreply.github.com>
2025-02-15 12:26:13 -0500

    Deprecating index.php. Trying in-line Python.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
 <title>Python Scripts</title>
@@ -5,3 +6,12 @@
 echo shell_exec("python test.py 'parameter1'");
 ?>
 </head>
+<body>
+	<h3>[Temporary] Click to run Python scripts: </h3>
+	<p>Script 1: <?php 
+	$script1 = escapeshellcmd('test.py');
+	$output = shell_exec($script1);
+	echo $output;
+?>
+</body>
+</html>
